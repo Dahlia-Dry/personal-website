40e578d7094c32531af68fcb154f57609fc83b24
Dahlia Dry <dahlia@dhcp-10-29-33-8.dyn.mit.edu>
2023-01-22 11:38:44 -0500

    changed static root

--- dahliaweb/settings.py
+++ dahliaweb/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'core/static')
+STATIC_ROOT = '/home/dahliadry/personal-website/core/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
